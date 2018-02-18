52556f2dc7078e72c72ef3a7ce9e37ae8b63e65e
JonLuca DeCaro <jdecaro@usc.edu>
2018-02-18 15:51:18 -0800

    section controller

--- server/controllers/ClassController.js
+++ server/controllers/ClassController.js
@@ -2,13 +2,35 @@ class ClassController {
   constructor(data) {
     if (data && data.OfferedCourses && data.OfferedCourses.course) {
       this.courseData = data.OfferedCourses.course;
+
+      /*Population section data*/
+      this.sections = {};
+
+      for (const course of this.courseData) {
+        let sectionData = course.CourseData.SectionData;
+
+        /*
+        * The USC API either returns an array of sections OR an object, if it's only a single section
+        * ...
+        *
+        * I know.
+        *
+        * */
+
+        if (Array.isArray(sectionData)) {
+          for (const section of sectionData) {
+            this.sections[section.id] = section;
+          }
+        } else {
+          this.sections[sectionData.id] = sectionData;
+        }
+      }
+
     }
   }
 
   getSection(sectionNumber) {
-    for (const course of this.courseData) {
-
-    }
+    return this.sections[sectionNumber];
   }
 
 }
