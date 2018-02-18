2d3c2eb912934909c21378ef18aec13dd577d399
JonLuca DeCaro <jdecaro@usc.edu>
2018-02-17 19:49:01 -0800

    cron work

---
+++ server/core/departmentRefresh.js
@@ -0,0 +1,54 @@
+const cron = require('cron');
+const StudentController = require("../controllers/StudentController");
+
+/*
+Cron job that runs every 15 minutes. Pulls newest classes
+Time interval changed to 1 hour during non-registration time
+*/
+
+cron.job("20 * * * * *", async () => {
+    departmentLog.info('Hard Class Refresh Starting');
+    refreshDepartments(await StudentController.getAllWatchedDepartments());
+}).start();
+
+function refreshDepartments(departments) {
+
+    //Headers so they know who's pulling their information
+    const headers = {
+        'DNT': '1',
+        'User-Agent': 'http://jonlu.ca/soc_api',
+        'Cache-Control': 'max-age=0'
+    };
+
+    let options = {
+        url: `http://web-app.usc.edu/web/soc/api/classes/20181?refresh=Mary4adAL1ttleLamp`,
+        headers,
+        timeout: 120000
+    };
+
+    for (const department of departments) {
+
+        options.url = `http://web-app.usc.edu/web/soc/api/classes/${department}/20181?refresh=Mary4adAL1ttleLamp`;
+
+        request(options, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                parseCourses(body);
+            }
+            else {
+                departmentLog.warn(`Error refreshing department ${department}`);
+                options.url = `http://web-app.usc.edu/web/soc/api/classes/${department}/20181`;
+                setTimeout(() => {
+                    request(options, (error, response, body) => {
+                        if (!error && response.statusCode === 200) {
+                            parseCourses(body);
+                        }
+                        else {
+                            departmentLog.error(`Error refreshing regular department ${department}`);
+                        }
+                    });
+                }, 30 * 1000);
+
+            }
+        });
+    }
+}
