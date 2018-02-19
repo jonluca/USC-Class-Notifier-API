569c5a10c22f25cfbe897bbd143483a1a3be7f0e
JonLuca DeCaro <jdecaro@usc.edu>
2018-02-18 17:39:46 -0800

    added text notifs

---
+++ server/controllers/TextController.js
@@ -0,0 +1,27 @@
+//Twilio variables
+const config = require("../config/config");
+const twilio = require('twilio');
+const accountSid = config.twilio.sid;
+const authToken = config.twilio.auth;
+const client = new twilio(accountSid, authToken);
+
+const logger = require('log4js').getLogger("notification");
+
+let TextController = {};
+
+TextController.sendMessage = (number, message) => {
+  if (number !== undefined && number !== "") {
+    client.messages.create({
+      body: message,
+      to: number,
+      from: '+16283333615'
+    }, (err, message) => {
+      if (err) {
+        logger.error(`Error sending text to: ${number} with error: ${err}`);
+        return;
+      }
+      logger.debug(`Sent text message to: ${message.to}`);
+    });
+  }
+};
+module.exports = TextController;
