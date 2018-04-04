10448fca50df1765a1b0a098cc7a47040b164fb3
JonLuca DeCaro <jdecaro@usc.edu>
2018-04-04 12:11:30 -0700

    paid emails

--- server/core/emailsWithTextNotifications.js
+++ server/core/emailsWithTextNotifications.js
@@ -1,8 +1,8 @@
 const emails = ["tranrl@usc.edu", "sarahyao@usc.edu", "anavbatr@usc.edu", "zack7siam@gmail.com", "zsiam@usc.edu", "gyashar@usc.edu", "sajeevsa@usc.edu", "javahera@usc.edu", "uyentuph@usc.edu", "mnmolaye@usc.edu",
-  "mart822@usc.edu", "julianamartinezb@gmail.com", "azanwar@usc.edu", "jwalrave@usc.edu"];
+    "mart822@usc.edu", "julianamartinezb@gmail.com", "azanwar@usc.edu", "jwalrave@usc.edu", "xmzzn123@gmail.com", "tpohlman@usc.edu", "johnduga@usc.edu"];
 
 function emailHasPaid(email) {
-  return emails.includes(email);
+    return emails.includes(email);
 }
 
 module.exports = emailHasPaid;
