82d29ef3f22d04d43e552509a2b80dc8847ddd8c
JonLuca DeCaro <jdecaro@usc.edu>
2018-04-09 11:55:58 -0700

    email

--- server/core/emailsWithTextNotifications.js
+++ server/core/emailsWithTextNotifications.js
@@ -1,6 +1,6 @@
 const emails = ["tranrl@usc.edu", "sarahyao@usc.edu", "anavbatr@usc.edu", "zack7siam@gmail.com", "zsiam@usc.edu", "gyashar@usc.edu", "sajeevsa@usc.edu", "javahera@usc.edu", "uyentuph@usc.edu", "mnmolaye@usc.edu",
     "mart822@usc.edu", "julianamartinezb@gmail.com", "azanwar@usc.edu", "jwalrave@usc.edu", "xmzzn123@gmail.com", "tpohlman@usc.edu", "johnduga@usc.edu", "tienrate@usc.edu", "pudidz@gmail.com", "abauman@usc.edu",
-    "sean@seangaul.com", "zoerberger@gmail.com", "caiyuliangbuzz@gmail.com", "sshipp@usc.edu", "amaripag@usc.edu", "adevgan@usc.edu", "woltersd@usc.edu", "millerbe@usc.edu", "janeth@usc.edu"];
+    "sean@seangaul.com", "zoerberger@gmail.com", "caiyuliangbuzz@gmail.com", "sshipp@usc.edu", "amaripag@usc.edu", "adevgan@usc.edu", "woltersd@usc.edu", "millerbe@usc.edu", "janeth@usc.edu", "ndelman@usc.edu"];
 
 function emailHasPaid(email) {
     return emails.includes(email);
