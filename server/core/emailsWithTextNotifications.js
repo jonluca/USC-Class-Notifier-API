ab93d7de49a9bcd22a9e57af298221f6eef511f9
JonLuca DeCaro <jdecaro@usc.edu>
2018-04-12 15:16:29 -0700

    email

--- server/core/emailsWithTextNotifications.js
+++ server/core/emailsWithTextNotifications.js
@@ -2,7 +2,7 @@ const emails = ["tranrl@usc.edu", "sarahyao@usc.edu", "anavbatr@usc.edu", "zack7
     "mart822@usc.edu", "julianamartinezb@gmail.com", "azanwar@usc.edu", "jwalrave@usc.edu", "xmzzn123@gmail.com", "tpohlman@usc.edu", "johnduga@usc.edu", "tienrate@usc.edu", "pudidz@gmail.com", "abauman@usc.edu",
     "sean@seangaul.com", "zoerberger@gmail.com", "caiyuliangbuzz@gmail.com", "sshipp@usc.edu", "amaripag@usc.edu", "adevgan@usc.edu", "woltersd@usc.edu", "millerbe@usc.edu", "janeth@usc.edu", "ndelman@usc.edu",
     "raywang@usc.edu", "gchrysta@usc.edu", "dhaddha@usc.edu", "jason2larue@gmail.com", "flawley@usc.edu", "ambhagat@usc.edu", "abhambha@usc.edu", "rpinckney5@gmail.com", "ntbasch@gmail.com", "graceeva@usc.edu",
-    "hxian@usc.edu", "mitchepl@usc.edu", "bryceste@usc.edu", "chanlee@usc.edu", "daniel@devereaux.cz", "eleblanc@usc.edu"];
+    "hxian@usc.edu", "mitchepl@usc.edu", "bryceste@usc.edu", "chanlee@usc.edu", "daniel@devereaux.cz", "eleblanc@usc.edu", "jpelliot@usc.edu"];
 
 function emailHasPaid(email) {
     return emails.includes(email);
