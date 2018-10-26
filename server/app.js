4dc7aabb30d0952c3e6add96ed520e263568c8bb
JonLuca DeCaro <jdecaro@usc.edu>
2018-10-25 20:51:40 -0400

    removed http basic logs

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const notify = require('./routes/notify');
@@ -13,7 +12,6 @@ require('./utils/security')(app);
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'ejs');
 
-app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
