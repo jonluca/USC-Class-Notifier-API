0704221208deda4351f421ab2a7430bb7db439f0
JonLuca DeCaro <jdecaro@usc.edu>
2018-12-08 11:44:23 +0100

    helper

--- server/core/emailsWithTextNotifications.js
+++ server/core/emailsWithTextNotifications.js
@@ -227,6 +227,11 @@ for (let i = 0; i < emails.length; i++) {
   emails[i] = emails[i].toLowerCase();
 }
 
+// Not used by SOCAPI, just helper functions for when I'm cleaning up the email list
+let dedupe = new Set(emails);
+let finalEmailList = Array.from(dedupe);
+finalEmailList = finalEmailList.sort();
+
 function emailHasPaid(email) {
   if (!email) {
     return false;
