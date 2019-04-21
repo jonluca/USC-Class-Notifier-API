a09a238993b57ec1fc1e8880e8be9898a725ebdd
JonLuca DeCaro <jdecaro@usc.edu>
2019-04-21 11:42:58 -0700

    'email'

--- server/core/emailsWithTextNotifications.js
+++ server/core/emailsWithTextNotifications.js
@@ -117,7 +117,8 @@ let emails = [
   "yunsungy@usc.edu",
   "zapolski@usc.edu",
   "zhuojunc@usc.edu",
-  "zhuyue@usc.edu"
+  "zhuyue@usc.edu",
+  "kavalent@usc.edu"
 ]
 
 for (let i = 0; i < emails.length; i++) {
