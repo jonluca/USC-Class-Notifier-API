c84b66eca52efd4fb7ece336fd76bda926fb53c2
JonLuca De Caro <jonlucadecaro96@gmail.com>
2019-07-31 14:46:01 -0700

    Update emailsWithTextNotifications.js

--- server/core/emailsWithTextNotifications.js
+++ server/core/emailsWithTextNotifications.js
@@ -248,7 +248,8 @@ let emails = [
   "zfujita@usc.edu",
   "zhuojunc@usc.edu",
   "zhuyue@usc.edu",
-  "zvogelsa@usc.edu"
+  "zvogelsa@usc.edu",
+  "rmmichel@usc.edu"
 ]
 
 for (let i = 0; i < emails.length; i++) {
