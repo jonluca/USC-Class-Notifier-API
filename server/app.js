911832b0d8ef411600c836230a8ff48c9f93564e
JonLuca DeCaro <jonluca.decaro@gmail.com>
2023-08-27 14:31:42 -0700

    enable cors

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const notify = require("./routes/notify");
 const admin = require("./routes/admin");
@@ -14,6 +15,7 @@ app.set("views", path.join(__dirname, "../views"));
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "../public")));
 app.use("/", notify);
