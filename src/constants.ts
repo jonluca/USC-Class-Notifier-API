38dc45abc688595757df2662a65d7dd577da7448
JonLuca DeCaro <jonluca.decaro@gmail.com>
2024-05-19 00:37:45 -0700

    is prod check

--- src/constants.ts
+++ src/constants.ts
@@ -1,1 +1,2 @@
 export const baseDomain = process.env.NODE_ENV === "production" ? "https://usc.jonlu.ca" : "http://localhost:3000";
+export const isProd = process.env.NODE_ENV === "production";
--- src/server/Twilio.ts
+++ src/server/Twilio.ts
@@ -1,6 +1,6 @@
 import tw from "twilio";
 import logger from "~/server/logger";
-import { isProd } from "~/server/config/logger";
+import { isProd } from "~/constants";
 const { Twilio } = tw;
 
 const accountSid = process.env.TWILIO_SID;
