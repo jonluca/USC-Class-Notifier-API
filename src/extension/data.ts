41f239688317ebf57100a6a8338ff1fa9633f66c
JonLuca DeCaro <jonluca.decaro@gmail.com>
2024-05-26 20:17:21 -0700

    fix baseurl

--- src/extension/data.ts
+++ src/extension/data.ts
@@ -2,13 +2,14 @@ import { httpLink } from "@trpc/client";
 import type { AppRouter } from "~/server/api";
 import { transformer } from "~/server/api/transformer";
 import { createTRPCReact } from "@trpc/react-query";
+import { baseDomain } from "~/constants";
 
 export const trpc = createTRPCReact<AppRouter>();
 
 export const trpcClient = trpc.createClient({
   links: [
     httpLink({
-      url: "http://localhost:3000/api/data",
+      url: `${baseDomain}/api/data`,
       transformer,
     }),
   ],
