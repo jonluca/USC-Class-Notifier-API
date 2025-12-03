c86d6bb8e69d844ee77da30e72d66afb9fb1bbbe
JonLuca DeCaro <jonluca.decaro@gmail.com>
2025-12-03 14:45:53 -0800

    deps

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,13 @@
 // @ts-check
 import "dotenv/config";
-/** @type {import('next').NextConfig} */
+import { type NextConfig } from "next";
 const nextConfig = {
   transpilePackages: ["@mui/icons-material", "@mui/material"],
   poweredByHeader: false,
   experimental: {
     largePageDataBytes: 100000000,
     optimizeServerReact: true,
-    appDocumentPreloading: true,
   },
-};
+} satisfies NextConfig;
 
 export default nextConfig;
