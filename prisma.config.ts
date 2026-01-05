64995ec0b2414543d3512d9e8ddf883330fccf5c
JonLuca DeCaro <jonluca.decaro@gmail.com>
2026-01-05 12:52:47 +0000

    update to not use env module

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,6 +4,6 @@ import { defineConfig, env } from "prisma/config";
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL_NON_POOLING || env("POSTGRES_PRISMA_URL"),
+    url: process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL_NON_POOLING,
   },
 });
