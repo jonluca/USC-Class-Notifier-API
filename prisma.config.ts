af5b5d971beee88266d2eea58895d892b2a04c0e
JonLuca DeCaro <jonluca.decaro@gmail.com>
2026-01-05 11:30:33 +0000

    create prisma config file

---
+++ prisma.config.ts
@@ -0,0 +1,9 @@
+import "dotenv/config";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: "prisma/schema.prisma",
+  datasource: {
+    url: process.env.POSTGRES_PRISMA_URL || process.env.POSTGRES_URL_NON_POOLING || env("POSTGRES_PRISMA_URL"),
+  },
+});
